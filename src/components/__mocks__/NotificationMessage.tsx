51766ccc8efb193ea18c8690639cf6ce16d44994
Eduardo Mohedano <emohedanomonter@gmail.com>
2021-02-19 00:40:16 -0600

    UI: Added unit tests for components

---
+++ src/components/__mocks__/NotificationMessage.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import {View} from 'react-native';
+
+const ComponentToMock = (props: any) => <View testID="notification-message__mock" {...props}></View>;
+export default ComponentToMock;
